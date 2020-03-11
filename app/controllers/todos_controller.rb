6618cdd052ea723a5b4016eb7b8bf3f370fae47c
vigneshiyer06 <vignesh.sankarn@gmail.com>
2020-03-11 18:12:07 +0530

    Added a method to take todos/id and render plain text with the input id

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,14 @@
 # todos_controller.rb
 class TodosController < ApplicationController
     def index
-      render plain: Todo.all.to_a
+      render plain: Todo.order(:due_date).map{|todo| todo.to_pleasant_string}.join("\n")
     end
+
+    def show 
+      id=params[:id]
+      render plain: "The id you wanted is #{id}"
+    end
+    
+
+
   end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,10 @@
 class Todo < ActiveRecord::Base
 
+    def to_pleasant_string
+        is_completed= completed ? "[X]" : "[ ]"
+        "#{id}. #{due_date.to_s(:short)} #{todo_text} #{is_completed}"
+        
+    end
+    
+
 end
