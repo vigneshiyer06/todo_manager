f85b1c7232bdfee2fe790e75e9fe9ba72286c066
vigneshiyer06 <vignesh.sankarn@gmail.com>
2020-03-11 20:59:28 +0530

    New create option was added

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -9,6 +9,19 @@ class TodosController < ApplicationController
       todo=Todo.find(id)
       render plain: todo.to_pleasant_string
     end
+    def create 
+      todo_text=params[:todo_text]
+      due_date=DateTime.parse(params[:due_date])
+      new_todo= Todo.create!(
+        todo_text: todo_text, 
+        due_date: due_date, 
+        completed: false
+      )
+      render plain: "Hey a new todo is create with the ID #{new_todo.id}"
+
+
+    end
+
 
 
 
