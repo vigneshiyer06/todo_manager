54c8143484bb3c0f93953c0df847da9d494bcda3
vigneshiyer06 <vignesh.sankarn@gmail.com>
2020-03-12 22:39:57 +0530

    added a create method to add a new user to the database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,23 @@ class UsersController < ApplicationController
     def index
         render plain: User.all.map{|user| user.to_pleasant_string}.join("\n")
     end
+    
     def show 
         id=params[:id]
         user=User.find(id)
         render plain: user.to_pleasant_string
+    end
+    
+    def create 
+        name=params[:name]
+        email=params[:email]
+        password=params[:password]
+        new_user= User.create!(
+          name: name, 
+          email: email, 
+          password: password
+        )
+        render plain: "Hey a new user is create with the ID #{new_user.id}"
     end
 
     
