097f75af9188058dcdf79ba461386dbf0f8e32d0
vigneshiyer06 <vignesh.sankarn@gmail.com>
2020-04-02 21:17:53 +0530

    Added forms functionality to add todo directly from the html

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,41 +1,37 @@
 # todos_controller.rb
 
 class TodosController < ApplicationController
-  
+  skip_before_action :verify_authenticity_token
 
-  skip_before_action  :verify_authenticity_token
-    def index
-      #render plain: Todo.order(:due_date).map{|todo| todo.to_pleasant_string}.join("\n")
-      render "index"
-    end
-
-    def show 
-      id=params[:id]
-      todo=Todo.find(id)
-      render "todo"
-      #render plain: todo.to_pleasant_string
-    end
-    def create 
-      todo_text=params[:todo_text]
-      due_date=DateTime.parse(params[:due_date])
-      new_todo= Todo.create!(
-        todo_text: todo_text, 
-        due_date: due_date, 
-        completed: false
-      )
-      render plain: "Hey a new todo is create with the ID #{new_todo.id}"
-
-    end
-    def update
-      id = params[:id]
-      completed = params[:completed]
-      todo = Todo.find(id)
-      todo.completed = completed
-      todo.save!
-      render plain: "Updated todo to status to #{completed}"
-    end
-
-
-
-
+  def index
+    #render plain: Todo.order(:due_date).map{|todo| todo.to_pleasant_string}.join("\n")
+    render "index"
   end
+
+  def show
+    id = params[:id]
+    todo = Todo.find(id)
+    render "todo"
+    #render plain: todo.to_pleasant_string
+  end
+
+  def create
+    todo_text = params[:todo_text]
+    due_date = DateTime.parse(params[:due_date])
+    new_todo = Todo.create!(
+      todo_text: todo_text,
+      due_date: due_date,
+      completed: false,
+    )
+    redirect_to todos_path
+  end
+
+  def update
+    id = params[:id]
+    completed = params[:completed]
+    todo = Todo.find(id)
+    todo.completed = completed
+    todo.save!
+    render plain: "Updated todo to status to #{completed}"
+  end
+end
