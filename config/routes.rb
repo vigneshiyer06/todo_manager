02a5444d11f2b6dbde8836ff46f2be3292d8ef58
vigneshiyer06 <vignesh.sankarn@gmail.com>
2020-04-12 19:39:33 +0400

    Added a sign out button for users to sign out

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   post "users/login", to: "users#login"
   get "/signin" , to:"sessions#new", as: :new_sessions
   post "/signin", to: "sessions#create", as: :sessions
+  delete "/signout", to:"sessions#destroy", as: :destroy_session
 
 
 end
