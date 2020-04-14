0efde821e9220df97168899bb15474a60f7a76a8
vigneshiyer06 <vignesh.sankarn@gmail.com>
2020-04-14 15:22:45 +0400

    Sign out functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,26 +1,23 @@
 class SessionsController < ApplicationController
-    skip_before_action :ensure_user_logged_in
-    def new
+  skip_before_action :ensure_user_logged_in
 
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:current_user_id] = user.id
+      redirect_to "/"
+    else
+      flash[:error] = "Your login attempt is Invaled. Please retry"
+      redirect_to new_sessions_path, warning: "Invalid User Id or Password"
     end
+  end
 
-    def create
-        user = User.find_by(email: params[:email])
-        if user && user.authenticate(params[:password])
-            session[:current_user_id] = user.id
-            redirect_to "/"
-        else
-            
-            redirect_to sessions_path, warning: "Invalid User Id or Password"
-            
-        end
-    end
-    
-    def destroy
-        session[:current_user_id]=nil
-        @current_user = nil
-        redirect_to "/"
-    end
-
-
-end  
+  def destroy
+    session[:current_user_id] = nil
+    @current_user = nil
+    redirect_to "/"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_12_083123) do
+ActiveRecord::Schema.define(version: 2020_04_14_065623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "menus", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "todos", force: :cascade do |t|
     t.text "todo_text"
@@ -29,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_04_12_083123) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "role"
   end
 
 end
